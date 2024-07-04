d062d7429795424dee179e201eca4cd6924c9f36
ahg611 <ahg611@naver.com>
2024-07-04 16:19:09 +0900

    Update final_app1.py

--- final_app1.py
+++ final_app1.py
@@ -23,7 +23,7 @@ import time
 import io
 
 # Set OpenAI API key
-openai.api_key = 'sk-proj-VgQ1S0ze3dZNgPAWa8Z5T3BlbkFJlqtH64kvMbeRDrUVNGgW'
+openai.api_key = 'yout openai api key'
 
 def initialize_session_state():
     """Initialize session state variables."""
